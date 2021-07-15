c782b86ff241cb22d104d3058bdd50b0a04f9306
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-07-15 14:27:26 +0100

    Implement BooksList, to display books as an html table

---
+++ src/containers/BooksList.js
@@ -0,0 +1,17 @@
+
+
+const BooksList = () => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Book Id</th>
+          <th>Title</th>
+          <th>Category</th>
+        </tr>
+      </thead>
+      <tbody>
+      </tbody>
+    </table>
+  )
+}
