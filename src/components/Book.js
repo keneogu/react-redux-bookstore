b62853f0c19196eaf928a77fbb0e8e54117bc2a9
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-07-16 23:44:45 +0100

    Add Book td to represent a single row in Books table

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,8 +1,19 @@
 import PropTypes from 'prop-types';
 
 const Book = ({ book: { bookId, title, category } }) => (
-  
+  <tr>
+    <td>{bookId}</td>
+    <td>{title}</td>
+    <td>{category}</td>
+  </tr>
 );
 
+Book.propTypes = {
+  book: PropTypes.shape({
+    bookId: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default Book;
