26f9820d45a742b617cc7cb7f452130a042d7671
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-07-17 13:42:02 +0100

    Add initial state object which contain few books

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
 import App from './components/App';
+import rootReducer from './reducers';
+
+const initialState = {
+  books: [
+    {
+      bookId: Math.floor(Math.random() * 10),
+      title: 'Mini habits',
+      category: 'History',
+    },
+    {
+      bookId: Math.floor(Math.random() * 10),
+      title: 'Deep work',
+      category: 'Biography',
+    },
+    {
+      bookId: Math.floor(Math.random() * 10),
+      title: 'Eat the frog',
+      category: 'Learning',
+    },
+  ],
+};
+
+const store = createStore(rootReducer, initialState);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+   
+      <App />
+ 
   </React.StrictMode>,
   document.getElementById('root'),
 );
