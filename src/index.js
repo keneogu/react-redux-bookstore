54f7473a68e5a18b15a1c0601026a0c052c1fd80
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-07-17 13:43:22 +0100

    Render App component wrapped in <Provider>

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
-
+import { Provider } from 'react-redux';
 import App from './components/App';
 import rootReducer from './reducers';
 
@@ -29,9 +29,9 @@ const store = createStore(rootReducer, initialState);
 
 ReactDOM.render(
   <React.StrictMode>
-   
+    <Provider store={store}>
       <App />
- 
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
