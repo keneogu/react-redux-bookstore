e5ad8b61422937664dc41227f219f5237d769847
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-07-19 22:57:21 +0100

    Implement handleChange method

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,20 +1,25 @@
 import React, { useState } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import createBook from '../actions/index';
 
 const BooksForm = (props) => {
   const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
 
   const [title, setTitle] = useState('');
   const [category, setCategory] = useState('');
-  
+
+  const handleChange = (e) => {
+    if (e.target.tagName === 'input') {
+      setTitle = e.target.value;
+    }else if (e.target.tagName === 'select') {
+      setCategory = e.target.value;
+    }
+  }
+
   return (
     <form>
-      <input type="text" placeholder="Book Form" />
-      <select name="categories" id="categories">
+      <input type="text" placeholder="Book Form" onChange={handleChange} value={title}/>
+      <select name="categories" id="categories" onChange={handleChange} value={category}>
         {categories.map((category) => (
-          <option key={Math.random} value={category}>
+          <option key={Math.random} value={category.id}>
             {category}
           </option>
         ))}
