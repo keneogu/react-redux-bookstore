5ed71da2f306d9b1520aa2be30445ecee6ff292f
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-07-20 12:20:45 +0100

    Add removeBook reducer

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -5,10 +5,7 @@ const booksReducer = (state = {}, action) => {
     case CREATE_BOOK:
       return [...state, { bookId: Math.floor(Math.random() * 1000), ...action.payload }];
     case REMOVE_BOOK:
-      return {
-        ...state,
-        books: state.filter((e) => e.id !== action.payload.bookId),
-      };
+      return state.filter((book) => book.bookId !== action.payload);
     default:
       return state;
   }
