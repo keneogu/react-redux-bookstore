b8c5a284004abdf7cf76d15f246a183e334d3657
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-07-21 22:58:16 +0100

    Return a select option with filter catgories

---
+++ src/components/CategoryFilter.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function CategoryFilter() {
+  const categories = ['All', 'Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+  
+  return (
+    <div>
+      <select>
+        {categories.map((category) => (
+          <option key={category} value={category}>{category}</option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+export default CategoryFilter;
