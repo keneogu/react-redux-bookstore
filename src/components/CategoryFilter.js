39f4de36814af66a56f7d7688c9839477cb62e9d
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-07-22 10:29:10 +0100

    Pass handleFilterChange as props in CategoryFilter

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function CategoryFilter() {
+function CategoryFilter(props) {
   const categories = ['All', 'Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
-
+  const { handleFilterChange } = props;
   return (
     <div>
-      <select>
+      <select onChange={handleFilterChange}>
         {categories.map((category) => (
           <option key={category} value={category}>{category}</option>
         ))}
@@ -13,5 +14,9 @@ function CategoryFilter() {
     </div>
   );
 }
+
+CategoryFilter.propTypes = {
+  handleFilterChange: PropTypes.func.isRequired,
+};
 
 export default CategoryFilter;
