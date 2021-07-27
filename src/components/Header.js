cdf0e5887ba3216faa2111aa532a2ca1f03aa4e2
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-07-27 16:08:50 +0100

    Add Header html files

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
+import CategoryFilter from './CategoryFilter';
+import '../styles/Header.css';
 
 function Header() {
-	return (
-		<div>
-			
-		</div>
-	)
+  return (
+    <div className="header">
+      <div className="head-text">
+        <h1 className="head-1">BookStore</h1>
+        <span className="head-span-1">Books</span>
+        <span className="head-span"><CategoryFilter /></span>
+      </div>
+      <div className="head-icon">
+        
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
