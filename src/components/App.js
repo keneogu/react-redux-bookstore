43fa573627ab0c7c4a99b3d616abf87bb951ed2c
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-07-27 15:33:15 +0100

    Add Header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import BooksForm from '../containers/BooksForm';
 import BooksList from '../containers/BooksList';
+import Header from './Header';
 import '../App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
       <BooksList />
       <BooksForm />
     </div>
