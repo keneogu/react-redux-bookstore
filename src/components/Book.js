f98c51d9d5c175e23abb82e9d78c1d2665c89dac
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-07-28 00:02:16 +0100

    Modify Book.js file

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,14 +1,39 @@
 import PropTypes from 'prop-types';
+import '../styles/Book.css';
 
 const Book = ({ book: { bookId, title, category }, handleRemoveBook }) => (
-  <tr>
-    <td>{bookId}</td>
-    <td>{title}</td>
-    <td>{category}</td>
-    <td>
-      <button type="button" onClick={() => handleRemoveBook(bookId)}>Remove Book</button>
-    </td>
-  </tr>
+
+  <div className="book">
+    <div className="book-left">
+      <div className="book_category">{category}</div>
+      <div className="book_title">{title}</div>
+      <div className="book_author">Author</div>
+      <div>
+        <span className="book_action">Comments</span>
+        <span className="book_action"><button className="normalize" type="button" onClick={() => handleRemoveBook(bookId)}>Remove</button></span>
+        <span className="book_action">Edit</span>
+      </div>
+    </div>
+    <div className="book-right">
+      <div className="box">
+        <div className="percent">
+          <svg>
+            <circle cx="34" cy="34" r="34" />
+            <circle cx="34" cy="34" r="34" />
+          </svg>
+        </div>
+      </div>
+      <div className="completion">
+        <div className="completion_number">64%</div>
+        <div className="completion_text">Completed</div>
+      </div>
+      <div className="chapter">
+        <div className="chapter_text">CURRENT CHAPTER</div>
+        <div className="chapter_number">Chapter 17</div>
+        <button type="button" className="chapter_button">UPDATE PROGRESS</button>
+      </div>
+    </div>
+  </div>
 );
 
 Book.propTypes = {
